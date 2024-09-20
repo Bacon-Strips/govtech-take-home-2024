1a547472fa0432812b8218a0b8885120c65385c9
Bacon-Strips <86605835+Bacon-Strips@users.noreply.github.com>
2024-09-20 21:08:27 +0800

    feat(backend): add connection to mongodb

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+
+dotenv.config({path: "../.env"});
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Error connecting to MongoDB:', err));
+
+
